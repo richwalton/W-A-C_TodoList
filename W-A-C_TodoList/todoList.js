9ff2e8a7748880fc354cb341ef6e8df568670484
Rich <sbtsun@yahoo.com>
2020-05-04 00:48:07 -0700

    Add new functions

--- W-A-C_TodoList/todoList.js
+++ W-A-C_TodoList/todoList.js
@@ -1,18 +1,32 @@
 let todoList = {
-  todos: ["item 1", "item 2", "item 3"],
-  displayTodos: function(){
+  todos: [],
+  displayTodos: function() {
     console.log("My Todos", this.todos);
   },
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos();
   },
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.toggleCompleted;
+    this.displayTodos();
   }
+  // my solution to above function is set position and booleen for method - toggleCompleted(0, true/false) 
+  // toggleCompleted: function(position, toggle) {
+  //   this.todos[position].completed = toggle;
+  //   this.displayTodos();
+  // }
 };
+console.log(todoList.todos)
